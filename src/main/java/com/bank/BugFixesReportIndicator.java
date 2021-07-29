388cf1d82bedc749a87c40b22bc3d3e94e4301e2
Shaiwal Sharma -X (shaiwsha - ZENSAR TECHNOLOGIES INC at Cisco) <shaiwsha@partnet.cisco.com>
2021-07-29 15:48:47 +0530

    First commit spring demo

---
+++ src/main/java/com/bank/BugFixesReportIndicator.java
@@ -0,0 +1,53 @@
+package com.bank;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.boot.actuate.endpoint.annotation.DeleteOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.boot.actuate.endpoint.annotation.WriteOperation;
+import org.springframework.stereotype.Component;
+
+@Component
+@Endpoint(id="bug-fixes")
+public class BugFixesReportIndicator {
+
+	static Map<String, List<String>> bugsMap = new HashMap<>();
+	
+	@PostConstruct
+	public void initialize() {
+		bugsMap.put("v1", Arrays.asList("Window close not working", "Browser extension not getting added"));
+		bugsMap.put("v2", Arrays.asList("OLX app is restarting", "OLX database is down"));
+	}
+	
+	@ReadOperation
+	public Map<String, List<String>> getBugFixesInfo() {
+		return bugsMap;
+	}
+	@ReadOperation
+	public List<String> getBugInfo(@Selector String version) {
+		return bugsMap.get(version);
+	}
+	@WriteOperation
+	public void addBug(@Selector String version, String bugs) {
+		bugsMap.put(version, Arrays.asList(bugs.split(",")));
+	}
+	@DeleteOperation
+	public void deleteBug(@Selector String version) {
+		bugsMap.remove(version);
+	}
+}
+//http://localhost:9000/olx-monitor/bug-fixes/v2 - GET -getBugInfo
+//http://localhost:9000/olx-monitor/bug-fixes - GET -getBugFixesInfo
+//http://localhost:9000/olx-monitor/bug-fixes/v3?bugs=First-bug,Second-bug - PODT and add content-type application/json - addBug
+//http://localhost:9000/olx-monitor/bug-fixes/v2 - DELETE - deleteBug
+
+
+
+
